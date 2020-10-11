18961d3495fd50b73695c96223bf9d6f8ba5ab25
Mai Pham <maitphm@gmail.com>
2020-10-11 15:16:25 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,7 +2,9 @@ def using_until
   levitation_force = 6
   
   until levitation_force == 10 
-    puts "wingardium Levi"
+    puts "wingardium Leviosa"
+    levitation_force += 1
+  end 
     
 end
 
